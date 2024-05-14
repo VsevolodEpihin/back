35702669439c7578665d3dc40912b55ee3b7b438
VsevolodEpihin <v.epihin@tg.dunice.net>
2024-05-14 09:53:15 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Task } from './tasks/task.model';
 import { TasksModule } from './tasks/tasks.module';
-
+ 
 @Module({
   imports: [
     ConfigModule.forRoot({
